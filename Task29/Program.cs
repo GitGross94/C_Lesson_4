aaf883697955b082abdb6a38c9e15e1ccff3b79c
Boris <twinlab.forever@yandex.ru>
2022-12-21 00:37:24 +0500

    Задача 29

---
+++ Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+    void arr()
+    {
+        int[] massiv = new int[8];
+        for (int i = 0; i < massiv.Length; i++)
+        {
+            Console.WriteLine($"Введите элемент массива под номером {i}.");
+            massiv[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+        Console.Write("Введеный массив: ");
+        for (int j = 0; j < massiv.Length; j++)
+        {
+            Console.Write($"{massiv[j]} ");
+        }
+    }
+    arr();
